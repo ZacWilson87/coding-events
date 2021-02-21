1084337b50bef0ea8431b5653c56b8e78fcfc927
Zac <zilson19878@gmail.com>
2021-02-21 10:38:57 -0600

    "add script.js and styles.css to project"

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +13,10 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private  static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Give Memphis");
-        events.add("Code Connector");
-        events.add("Code With Pride");
-        events.add("Tech Smasher");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,4 +26,10 @@ public class EventController {
         return "events/create";
     }
 
+    // lives at /events/create`
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
 }
